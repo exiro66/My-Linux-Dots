2cd92100372fa0fb161654b47771b596bed5cab5
exiro66 <exiro66@users.noreply.github.com>
2026-09-17 17:44:05 +0100

    Hyprlock minimalista + bind SUPER+L

--- .config/hypr/config/binds.lua
+++ .config/hypr/config/binds.lua
@@ -75,6 +75,7 @@ hl.bind(mod .. " + SHIFT + Left", hl.dsp.exec_cmd(tide .. " tide swipeLeft"))
 hl.bind(mod .. " + SHIFT + Right", hl.dsp.exec_cmd(tide .. " tide swipeRight"))
 hl.bind(mod .. " + R", hl.dsp.exec_cmd("systemctl --user restart tide-island"))
 hl.bind(mod .. " + SHIFT + R", hl.dsp.exec_cmd(tide .. " tide showTimer"))
+hl.bind(mod .. " + L", hl.dsp.exec_cmd("hyprlock"))
 
 -- ============================================================
 -- HYPRLAND
