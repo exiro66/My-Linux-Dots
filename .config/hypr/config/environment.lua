300e6e056c8f273fd6bd6598e3124517bc11fadb
exiro66 <exiro66@users.noreply.github.com>
2026-09-17 17:26:27 +0100

    Config completa: Tide Island + pywal + colores dinámicos + TLP

--- .config/hypr/config/environment.lua
+++ .config/hypr/config/environment.lua
@@ -4,6 +4,7 @@
 
 -- if you have an NVIDIA GPU uncomment the following lines:
 
+hl.env("QS_ICON_THEME", "MacTahoe")
 -- hl.env("GBM_BACKEND", "nvidia-drm") -- force GBM as a backend
 -- hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia") -- force GBM as a backend
 -- hl.env("LIBVA_DRIVER_NAME", "nvidia") -- Hardware acceleration on NVIDIA GPUs
